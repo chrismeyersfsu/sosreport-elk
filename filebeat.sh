d3ec78714e285078a32bb98d4681f42459632fc7
Chris Meyers <chris.meyers.fsu@gmail.com>
2020-11-08 13:59:01 -0500

    relax permissions

--- filebeat.sh
+++ filebeat.sh
@@ -2,4 +2,4 @@
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' elk:9200)" != "200" ]]; do sleep 1; done
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' elk:5601/app/home/)" != "200" ]]; do sleep 1; done
 filebeat setup
-filebeat run
+filebeat --strict.perms=false run
