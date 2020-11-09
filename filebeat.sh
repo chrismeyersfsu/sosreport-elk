cdad7676ef56b7b008178d72d1154d0380e940ac
Chris Meyers <chris.meyers.fsu@gmail.com>
2020-11-09 16:01:47 -0500

    run once optimization doesn't work
    * Something to do with the close_eof kind of parameters

--- filebeat.sh
+++ filebeat.sh
@@ -2,6 +2,6 @@
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' elk:9200)" != "200" ]]; do sleep 1; done
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' elk:5601/app/home/)" != "200" ]]; do sleep 1; done
 filebeat --strict.perms=false setup --pipelines
-filebeat --strict.perms=false run --once
 filebeat --strict.perms=false setup --dashboards
+filebeat --strict.perms=false run
 echo "Successfully processed log files, exiting now!"
