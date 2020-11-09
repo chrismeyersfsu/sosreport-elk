7bf48ad8fef7ded99c03d7a39017f0a908d0349a
Chris Meyers <chris.meyers.fsu@gmail.com>
2020-11-09 11:46:18 -0500

    simplied config, sped up init processing, perm fix
    * Run docker containers as current user. Should fix simple file access
    issues.
    * Only run filebeat once instead of continuing to run
    * simplify filebeat.yml config

--- filebeat.sh
+++ filebeat.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' elk:9200)" != "200" ]]; do sleep 1; done
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' elk:5601/app/home/)" != "200" ]]; do sleep 1; done
-filebeat --strict.perms=false setup
-filebeat --strict.perms=false run
+filebeat --strict.perms=false setup --pipelines
+filebeat --strict.perms=false run --once
+filebeat --strict.perms=false setup --dashboards
+echo "Successfully processed log files, exiting now!"
