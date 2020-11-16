54c24568ccd4c69be32827c842d2dc64f9ff4147
Chris Meyers <chris.meyers.fsu@gmail.com>
2020-11-16 09:10:19 -0500

    faster bring up time
    * Remove unused dashboards
    * Disable erroring browser plugin

--- filebeat.sh
+++ filebeat.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+find kibana/7/dashboard/ -type f -not -name '*nginx*' -not -name '*tower*' -delete
+
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' elk:9200)" != "200" ]]; do sleep 1; done
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' elk:5601/app/home/)" != "200" ]]; do sleep 1; done
 filebeat --strict.perms=false setup --pipelines
